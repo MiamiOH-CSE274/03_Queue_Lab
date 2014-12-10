53c1d6004f88368897c92fe5d81972d8193f98e0
GrasysER <grasyser@miamioh.edu>
2014-12-10 16:16:31 -0500

    Formatting fixes

--- ArrayQueue.h
+++ ArrayQueue.h
@@ -4,43 +4,43 @@
 #include "Queue.h"
 
 template <class T>
-class ArrayQueue : public Queue <T> {
- public:
-  //See Queue.h for documentation of these methods
+class ArrayQueue : public Queue < T > {
+public:
+	//See Queue.h for documentation of these methods
 
-  //This class MUST use the "circular" array concept,
-  // so if I do add, remove, add, remove, add, remove ...
-  // even if I do it 10000000 times, your array should not grow.
-  // The array should never grow unless numItems == backingArraySize
-  void add(T toAdd);
-  T remove();
-  unsigned long getNumItems();
-  
-  //Initialize all private member variables.
-  // You initial backing array should be length 10. Allocate it
-  // using "new"
-  ArrayQueue();
-  //Delete any dynamically allocated memory. If you are deleting
-  // an array, be sure to use "delete[]" instead of "delete"
-  virtual ~ArrayQueue();
+	//This class MUST use the "circular" array concept,
+	// so if I do add, remove, add, remove, add, remove ...
+	// even if I do it 10000000 times, your array should not grow.
+	// The array should never grow unless numItems == backingArraySize
+	virtual  void add(T toAdd);
+	virtual T remove();
+	virtual unsigned long getNumItems();
 
- private:
-  //If the backing array is too small, you may call this function.
-  // It should create a new backing array twice as big as the old one,
-  // and copy over the old data into the new array. It should finish
-  // off by calling delete[] on the old backing array
-  void grow();
+	//Initialize all private member variables.
+	// You initial backing array should be length 10. Allocate it
+	// using "new"
+	ArrayQueue();
+	//Delete any dynamically allocated memory. If you are deleting
+	// an array, be sure to use "delete[]" instead of "delete"
+	virtual ~ArrayQueue();
 
-  //Pointer to the array that we are using to store the queue
-  T* backingArray;
-  
-  //The index in T where the front of the queue is.
-  unsigned long front;
-  //Number of items in the queue
-  unsigned long numItems;
+private:
+	//If the backing array is too small, you may call this function.
+	// It should create a new backing array twice as big as the old one,
+	// and copy over the old data into the new array. It should finish
+	// off by calling delete[] on the old backing array
+	void grow();
 
-  //Number of spaces in the array pointed to by backingArray
-  unsigned long backingArraySize;
+	//Pointer to the array that we are using to store the queue
+	T* backingArray;
+
+	//The index in T where the front of the queue is.
+	unsigned long front;
+	//Number of items in the queue
+	unsigned long numItems;
+
+	//Number of spaces in the array pointed to by backingArray
+	unsigned long backingArraySize;
 };
 
 /***
@@ -73,54 +73,51 @@ class ArrayQueue : public Queue <T> {
 
 template <class T>
 ArrayQueue<T>::ArrayQueue(){
-numItems = 0;
-front = 0;
-backingArraySize = START_SIZE;
-backingArray = new T[backingArraySize];
+	numItems = 0;
+	front = 0;
+	backingArraySize = START_SIZE;
+	backingArray = new T[backingArraySize];
 }
 
 
 template <class T>
 ArrayQueue<T>::~ArrayQueue() {
-delete[] backingArray;
-backingArray = NULL;
+	delete[] backingArray;
+	backingArray = NULL;
 }
 
 template <class T>
 void ArrayQueue<T>::add(T toAdd){
 
+	//grow array if full
+	if (numItems == backingArraySize)
+	{
+		grow();
+	}
+
+	//else add toAdd to the first available spot, modulo code from lecture
+	backingArray[((front + numItems) % backingArraySize)] = toAdd;
 	numItems++;
-//grow array if full
-if (numItems = backingArraySize)
-{
-grow();
-}
-
-//else add toAdd to the first available spot, modulo code from lecture
-backingArray[(front+numItems) % backingArraySize] = toAdd;
-
 }
 
 
 template <class T>
 T ArrayQueue<T>::remove(){
-if (numItems < 1)
-{
-//bad news
-throw std::string("Queue is already empty, attempted to remove.");
+	if (numItems < 1)
+	{
+		//bad news
+		throw std::string("Queue is already empty, attempted to remove.");
+	}
+
+	T retVal = backingArray[front];
+	front = (front + 1) % backingArraySize;
+	numItems--;
+
+	return retVal;
+
+
 }
 
-numItems--;
-
-T retVal = backingArray[front];
-front = (front + 1) % backingArraySize;
-
-
-return retVal;
-
-
-}     
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 
 template <class T>
 unsigned long ArrayQueue<T>::getNumItems(){
@@ -131,17 +128,17 @@ unsigned long ArrayQueue<T>::getNumItems(){
 template <class T>
 void ArrayQueue<T>::grow(){
 
-T* biggerArray = new T[backingArraySize*2];
+	T* biggerArray = new T[backingArraySize * 2];
 
-//copy over old values
-for (unsigned int i = 0; i < numItems; i++){
-biggerArray[i] = backingArray[(i+front)% backingArraySize];
-}
+	//copy over old values
+	for (unsigned int i = 0; i < numItems; i++){
+		biggerArray[i] = backingArray[(i + front) % backingArraySize];
+	}
 
-front = 0;
-delete[] backingArray;
-backingArray = biggerArray;
-backingArraySize = (backingArraySize*2);
+	front = 0;
+	delete[] backingArray;
+	backingArray = biggerArray;
+	backingArraySize = (backingArraySize * 2);
 
 
 }
